dfa90c5b405630ffd5923094f4e2afa46866aedd
Sriyan-Nakka <sriyannakka@gmail.com>
2025-05-31 19:15:54 +0530

    version 0.4

---
+++ script.js
@@ -0,0 +1,26 @@
+let cardOrder = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+for (let rep = 1; rep <= 20; rep++) {
+  let a = Math.floor(Math.random() * cardOrder.length);
+  let b = Math.floor(Math.random() * cardOrder.length);
+
+  let temp = cardOrder[a];
+  cardOrder[a] = cardOrder[b];
+  cardOrder[b] = temp;
+}
+console.log(cardOrder);
+
+document.querySelectorAll(".card").forEach((card) => {
+  card.addEventListener("click", () => {
+    card.classList.toggle("cardReveal");
+    card.style.pointerEvents = "none";
+    console.log(card.dataset.shape);
+
+    //todo add when you reach 2 of a variable name, results show.
+  });
+});
+function closeOpenCards() {
+  document.querySelectorAll(".card").forEach((card) => {
+    card.classList.remove("cardReveal");
+  });
+}
