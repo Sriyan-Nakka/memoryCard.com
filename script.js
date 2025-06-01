37e397debd7d08f8bc43a869d22c383bd8890977
Sriyan-Nakka <sriyannakka@gmail.com>
2025-06-01 22:31:27 +0530

    version 0.49

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let cardOrder = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+let cardFlip = 0;
 
+//cardOrder array Shuffle
 for (let rep = 1; rep <= 20; rep++) {
   let a = Math.floor(Math.random() * cardOrder.length);
   let b = Math.floor(Math.random() * cardOrder.length);
@@ -15,8 +17,8 @@ document.querySelectorAll(".card").forEach((card) => {
     card.classList.toggle("cardReveal");
     card.style.pointerEvents = "none";
     console.log(card.dataset.shape);
-
-    //todo add when you reach 2 of a variable name, results show.
+    cardFlip++;
+    //todo: add when you reach 2 of a variable name, results show.
   });
 });
 function closeOpenCards() {
